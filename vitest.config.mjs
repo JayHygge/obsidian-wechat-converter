173d9b5d75296c7b45f3f7cd396481e28edf990b
DavidLam <linanyou@gmail.com>
2026-02-06 19:26:59 +0800

    fix(ci): move alias config to resolve.alias scope to correctly mock obsidian module

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -5,12 +5,14 @@ import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  test: {
-    environment: 'jsdom',
-    include: ['tests/**/*.test.js'],
+  resolve: {
     alias: {
       obsidian: path.resolve(__dirname, './__mocks__/obsidian.js'),
     },
+  },
+  test: {
+    environment: 'jsdom',
+    include: ['tests/**/*.test.js'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
